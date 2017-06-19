180f3f13800bfe8079a35847dd4fdd4a7b40f85a
revathich21 <revathi.kpf303@gmail.com>
2017-06-19 14:28:51 -0500

    Update and rename main.md to main.py

--- Assignments/program_2/main.py
+++ Assignments/program_2/main.py
@@ -1,4 +1,4 @@
-'''py
+
 """
 Program:
 --------
@@ -138,5 +138,5 @@ if __name__ == '__main__':
             pygame.draw.circle(screen, (253,182,50), p4, 3, 0) #island
         
         pygame.display.flip()
-    '''
+    
    
