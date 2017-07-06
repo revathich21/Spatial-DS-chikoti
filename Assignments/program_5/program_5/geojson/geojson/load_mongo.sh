c6b516fc5adcbb6c1bdd06b50575f76d1a97b917
anusha mongolu <anusha mongolu>
2017-07-05 22:15:58 -0500

    Del

--- Assignments/program_5/program_5/geojson/geojson/load_mongo.sh
+++
@@ -1,9 +0,0 @@
-mongo world_data --eval "db.dropDatabase()"
-mongoimport --db world_data --collection airport       --type json --file airport.geojson        --jsonArray
-mongoimport --db world_data --collection countries      --type json --file countries.geojson       --jsonArray
-mongoimport --db world_data --collection meteorite     --type json --file meteorite.geojson       --jsonArray
-mongoimport --db world_data --collection volcanos       --type json --file volcanos.geojson        --jsonArray
-mongoimport --db world_data --collection earthquakes    --type json --file earthquakes.geojson     --jsonArray
-mongoimport --db world_data --collection cities         --type json --file world_cities.geojson    --jsonArray
-mongoimport --db world_data --collection states         --type json --file state_borders.geojson   --jsonArray
-mongoimport --db world_data --collection globalterrorism      --type json --file globalterrorism.geojson --jsonArray
